bcd83c94449363b997936eb4972852f76bdbce77
Ruth <rutsanmun@gmail.com>
2019-11-24 19:41:15 +0100

    Manejo errores y validación formulario - Libro
    Secciones  7, 8 y 9

--- clientes-app/src/app/clientes/cliente.service.ts
+++ clientes-app/src/app/clientes/cliente.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { CLIENTES } from './cliente.json';
 import { Cliente } from './cliente'; //importamos la clase
-import { of, Observable } from 'rxjs';
+import { of, Observable, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
+import swal from 'sweetalert2';
 
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +16,7 @@ export class ClienteService {
   private urlEndPoint:string = "http://localhost:8080/api/clientes";
 
   private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'})
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router : Router) { }
 
   getClientes(): Observable<Cliente[]>{
     //return of(CLIENTES);
@@ -25,18 +27,50 @@ export class ClienteService {
   }
 
   create(cliente: Cliente) : Observable<Cliente> {
-    return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.httpHeaders});
+    return this.http.post(this.urlEndPoint, cliente, {headers: this.httpHeaders}).pipe(
+      map( (response: any) => response.cliente as Cliente),
+      catchError(e => {
+        if(e.status ==400){
+          return throwError(e);
+        }
+        console.error(e.error.mensaje);
+        swal('Error al crear', e.error.error, 'error');
+        return throwError(e);
+      })
+    );
   }
 
   getCliente(id) : Observable<Cliente>{
-    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`)
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
+      catchError (e => {
+        this.router.navigate(['/clientes']);
+        console.error(e.error.mensaje);
+        swal('Error al editar', e.error.mensaje, 'error');
+        return throwError(e);
+      })
+    );
   }
 
-  update(cliente : Cliente) : Observable<Cliente>{
-    return this.http.put<Cliente>(`${this.urlEndPoint}/${cliente.id}`, cliente, {headers: this.httpHeaders});
+  update(cliente : Cliente) : Observable<any>{
+    return this.http.put<any>(`${this.urlEndPoint}/${cliente.id}`, cliente, {headers: this.httpHeaders}).pipe(
+      catchError(e => {
+        if(e.status ==400){
+          return throwError(e);
+        }
+        console.error(e.error.mensaje);
+        swal('Error al crear', e.error.error, 'error');
+        return throwError(e);
+      })
+    );
   }
 
   delete(id) :Observable<Cliente>{
-    return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`, {headers : this.httpHeaders})
+    return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`, {headers : this.httpHeaders}).pipe(
+      catchError(e => {
+        console.error(e.error.mensaje);
+        swal('Error al crear', e.error.error, 'error');
+        return throwError(e);
+      })
+    );
   }
 }
